9403c86bfacd8cab218f57d8990e97b14f6530d4
Marina <example@gmail.com>
2021-04-23 13:08:13 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log (number);
+
+/* leftBorderWidth = 4; */
+const obj = {
+    a: 50
+};
+
+obj.a = 1;
+console.log (obj);
+
+console.log (a);
+var a = 5;
+
+let j = 1;
+console.log (4/0);
+
+let m = Number.MIN_SAFE_INTEGER;
+console.log(m);
+
+let b = prompt('вопрос', '');
+console.log (b);
+console.log(typeof b);
+
+console.log(`первая строка 
+вторая строка`);
+
+const isChecked = true,
+exemple = true,
+isClose = true;
+
+console.log (isChecked && isClose && exemple);
